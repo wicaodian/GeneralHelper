91c4644671eadcd0e34404a8a81014f57c38cca9
Asad Mehmood <wicaodian@gmail.com>
2024-02-29 11:15:40 +0500

    Random Password Generation Added

--- generalHelper/GeneralHelper.kt
+++ generalHelper/GeneralHelper.kt
@@ -80,4 +80,57 @@ object GeneralHelper {
                 capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) ||
                 capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)
     }
+
+    /**
+     * Generates a one-time password (OTP) with given specifications.
+     *
+     * @param passwordLength The length of the generated password. Defaults to 8 characters.
+     * @param shuffleCharacters Flag to determine if the character set should be shuffled. Defaults to true.
+     * @param random An instance of SecureRandom for cryptographic strength randomness. Defaults to a new instance.
+     * @param includeLowercase Flag to include lowercase letters in the character set. Defaults to true.
+     * @param includeUppercase Flag to include uppercase letters in the character set. Defaults to true.
+     * @param includeDigits Flag to include digits in the character set. Defaults to true.
+     * @param includeSpecialChars Flag to include special characters in the character set. Defaults to true.
+     * @param exemptChars A list of characters to exclude from the password. Defaults to an empty list.
+     * @return A string representing the generated one-time password.
+     */
+    fun generateRandomPassword(
+            passwordLength: Int = 8,
+            shuffleCharacters: Boolean = true,
+            random: SecureRandom = SecureRandom(),
+            exemptChars: List<Char> = emptyList(),
+            includeLowercase: Boolean = true,
+            includeUppercase: Boolean = true,
+            includeDigits: Boolean = true,
+            includeSpecialChars: Boolean = true,
+            exemptChars: List<Char> = emptyList()
+                               ): String {
+        // Define character categories
+        val lowercaseLetters = "abcdefghijklmnopqrstuvwxyz"
+        val uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        val digits = "0123456789"
+        val specialChars = "!@%$%&^?|~'\"#+=\\*/.,:;[]()-_<>"
+
+        // Build the character set based on the flags provided
+        val characterSet = StringBuilder().apply {
+            if (includeLowercase) append(lowercaseLetters)
+            if (includeUppercase) append(uppercaseLetters)
+            if (includeDigits) append(digits)
+            if (includeSpecialChars) append(specialChars)
+        }.toString().toCharArray().toMutableList()
+
+        // Shuffle the character set if required
+        if (shuffleCharacters) {
+            characterSet.shuffle(random)
+        }
+
+        // Remove exempt characters
+        characterSet.removeAll(exemptChars)
+
+        // Generate the password
+        return (1..passwordLength)
+            .map { characterSet[random.nextInt(characterSet.size)] }
+            .joinToString("")
+    }
+
 }
